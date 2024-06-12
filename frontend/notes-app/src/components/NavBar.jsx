445f87c342a4664c849ce0f88cc0b34808cba031
wa1ead <waliddraa2@gmail.com>
2024-06-12 17:00:31 +0100

    routes: react-router-dom

---
+++ frontend/notes-app/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+      <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+    </div>
+  );
+};
+
+export default NavBar;
--- frontend/notes-app/tailwind.config.js
+++ frontend/notes-app/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2B85FF",
+        secondary: "#EF863E",
+      },
+    },
   },
   plugins: [],
 };
