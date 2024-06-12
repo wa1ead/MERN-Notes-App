c85130b0bdd8f1b0140d2de4bbe29cddff336332
wa1ead <waliddraa2@gmail.com>
2024-06-12 17:01:21 +0100

    login form: TailwindCSS

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import NavBar from "../../components/NavBar";
+import PasswordInput from "../../components/input/PasswordInput";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <NavBar />
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={() => {}}>
+            <h4 className="text-2xl mb-7"></h4>
+            <input type="text" placeholder="email" className="input-box" />
+            <PasswordInput />
+            <button type="submit" className="btn-primary">
+              {" "}
+              Login
+            </button>
+            <p className="text-sm text-center mt-4">
+              Not registrated yet?{" "}
+              <Link to="/signup" className="font-medium text-primary underline">
+                Create an Account
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
