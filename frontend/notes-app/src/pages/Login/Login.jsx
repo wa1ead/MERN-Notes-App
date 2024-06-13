3651380912fcdcc3df5560eea1d7903333c001dd
wa1ead <waliddraa2@gmail.com>
2024-06-13 18:33:55 +0100

    signup form: TailwindCSS

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import NavBar from "../../components/NavBar";
+import NavBar from "../../components/NavBar/NavBar";
 import PasswordInput from "../../components/input/PasswordInput";
+import { validateEmail } from "../../utils/helper";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    if (!validateEmail(email)) {
+      setError("Please enter a valid Email address.");
+      return;
+    }
+    if (!password) {
+      setError("Please enter your Password.");
+      return;
+    }
+    setError("");
+  };
+
   return (
     <>
       <NavBar />
       <div className="flex items-center justify-center mt-28">
         <div className="w-96 border rounded bg-white px-7 py-10">
-          <form onSubmit={() => {}}>
-            <h4 className="text-2xl mb-7"></h4>
-            <input type="text" placeholder="email" className="input-box" />
-            <PasswordInput />
+          <form onSubmit={handleLogin}>
+            <h4 className="text-2xl mb-7">Login</h4>
+            <input
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="input-box"
+            />
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+
             <button type="submit" className="btn-primary">
               {" "}
               Login
--- frontend/notes-app/src/pages/SignUp/SignUp.jsx
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { validateEmail } from "../../utils/helper";
+import NavBar from "../../components/NavBar/NavBar";
+import PasswordInput from "../../components/input/PasswordInput";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    if (!name) {
+      setError("Please enter your Name.");
+      return;
+    }
+    if (!validateEmail(email)) {
+      setError("Please enter a valid Email address.");
+      return;
+    }
+    if (!password) {
+      setError("Please enter a Password.");
+      return;
+    }
+    setError("");
+  };
+
+  return (
+    <>
+      <NavBar />
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={handleSignUp}>
+            <h4 className="text-2xl mb-7">Sign Up</h4>
+            <input
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="input-box"
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+
+            <button type="submit" className="btn-primary">
+              Create Account
+            </button>
+            <p className="text-sm text-center mt-4">
+              Already have an account?{" "}
+              <Link to="/login" className="font-medium text-primary underline">
+                Login
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SignUp;
