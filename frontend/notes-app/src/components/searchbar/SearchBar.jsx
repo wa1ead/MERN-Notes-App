e0547fa72b0bc080ef733f065fef742d9312fab3
wa1ead <waliddraa2@gmail.com>
2024-06-14 16:04:45 +0100

    search bar component

---
+++ frontend/notes-app/src/components/searchbar/SearchBar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { IoMdClose } from "react-icons/io";
+
+const SearchBar = ({ value, onChange, handleSearch, onClearSearch }) => {
+  return (
+    <div className="w-80 flex items-center px-4 bg-slate-100 rounded-md">
+      <input
+        type="text"
+        placeholder="Search Notes"
+        className="w-full text-xs bg-transparent outline-none py-[11px]"
+        value={value}
+        onChange={onChange}
+      />
+      {value && (
+        <IoMdClose
+          className="text-xl text-slate-500 cursor-pointer hover:text-black mr-3"
+          onClick={onClearSearch}
+        />
+      )}
+      <FaMagnifyingGlass
+        className="text-slate-400 cursor-pointer hover:text-black"
+        onClick={handleSearch}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
