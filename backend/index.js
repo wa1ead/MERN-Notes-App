b06499ae3028583316773363c2a7bfcb53c4c6a9
wa1ead <waliddraa2@gmail.com>
2024-06-21 16:59:15 +0100

    create account: API

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const config = require("./config.json");
 const mongoose = require("mongoose");
 
 mongoose.connect(config.connectionString);
+const User = require("./models/user.model");
 
 const express = require("express");
 const cors = require("cors");
@@ -21,6 +22,52 @@ app.use(
 
 app.get("/", (req, res) => {
   res.json({ data: "hello" });
+});
+
+//CREATE ACCOUNT
+app.post("/create-account", async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  if (!fullName) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Full Name is required!" });
+  }
+
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required!" });
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required!" });
+  }
+
+  const isUser = await User.findOne({ email: email });
+
+  if (isUser) {
+    return res.json({ error: true, message: "User already exists!" });
+  }
+
+  const user = new User({
+    fullName,
+    email,
+    password,
+  });
+
+  await user.save();
+
+  const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "36000m",
+  });
+
+  return res.json({
+    error: false,
+    user,
+    accessToken,
+    message: "Registration successful!",
+  });
 });
 
 app.listen(8000);
