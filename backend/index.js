60591d7d33f1c5ba392b928111d453f554599e2f
wa1ead <waliddraa2@gmail.com>
2024-06-21 16:43:43 +0100

    database connection: mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
+require("dotenv").config();
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.connectionString);
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./utilities");
 
 app.use(express.json());
 
