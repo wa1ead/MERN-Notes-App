eba4201377df0d590f501fecb8970146c63926e3
wa1ead <waliddraa2@gmail.com>
2024-06-22 16:52:57 +0100

    login: API

--- backend/index.js
+++ backend/index.js
@@ -70,6 +70,41 @@ app.post("/create-account", async (req, res) => {
   });
 });
 
+//LOGIN
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ message: "Email is required!" });
+  }
+
+  if (!password) {
+    return res.status(400).json({ message: "Password is required!" });
+  }
+
+  const userInfo = await User.findOne({ email: email });
+
+  if (!userInfo) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  if (userInfo.email == email && userInfo.password == password) {
+    const user = { user: userInfo };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "36000m",
+    });
+
+    return res.json({
+      error: false,
+      message: "Login successful!",
+      email,
+      accessToken,
+    });
+  } else {
+    return res.json({ error: true, message: "Invalid Cardentials" });
+  }
+});
+
 app.listen(8000);
 
 module.exports = app;
