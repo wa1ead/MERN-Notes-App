a40df1e8b80575e162876fbdabdbcb20de68faed
wa1ead <waliddraa2@gmail.com>
2024-06-24 13:14:25 +0100

    add new note: mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
 const config = require("./config.json");
 const mongoose = require("mongoose");
-
 mongoose.connect(config.connectionString);
-const User = require("./models/user.model");
 
 const express = require("express");
 const cors = require("cors");
@@ -11,6 +9,9 @@ const app = express();
 
 const jwt = require("jsonwebtoken");
 const { authenticateToken } = require("./utilities");
+
+const User = require("./models/user.model");
+const Note = require("./models/note.model");
 
 app.use(express.json());
 
@@ -102,6 +103,39 @@ app.post("/login", async (req, res) => {
     });
   } else {
     return res.json({ error: true, message: "Invalid Cardentials" });
+  }
+});
+
+//ADD NOTE
+app.post("/add-note", authenticateToken, async (req, res) => {
+  const { title, content, tags } = req.body;
+  const { user } = req.user;
+
+  if (!title) {
+    return res.status(400).json({ error: true, message: "Title is required!" });
+  }
+
+  if (!content) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Content is required!" });
+  }
+
+  try {
+    const note = new Note({
+      title,
+      content,
+      tags: tags || [],
+      userId: user._id,
+    });
+    await note.save();
+    return res
+      .status(400)
+      .json({ error: false, message: "Note created successfully!", note });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: true, message: "Internal Server Error" });
   }
 });
 
